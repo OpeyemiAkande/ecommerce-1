57419298ad18602d4db0163a2ab88de636440bbf
Opeyemi Akande <akande212@gmail.com>
2026-03-22 15:21:46 +0100

    updated admin folder

---
+++ admin/src/App.jsx
@@ -0,0 +1,12 @@
+import Example from "./components/Example";
+
+function App() {
+  return (
+    <div>
+      <h1>Vite + React App</h1>
+      <Example />
+    </div>
+  );
+}
+
+export default App;
