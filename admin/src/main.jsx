f26adf0424ebc618753cab39932675dbd29dd373
Opeyemi Akande <akande212@gmail.com>
2026-03-29 06:25:50 +0100

    Dashboard page ready on admin

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -2,19 +2,39 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import {ClerkProvider} from "@clerk/react";
+
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+
+import {ClerkProvider} from "@clerk/clerk-react";
+import {BrowserRouter} from "react-router";
+import * as Sentry from "@sentry/react";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-console.log(PUBLISHABLE_KEY);
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
 }
 
+console.log(PUBLISHABLE_KEY);
+const queryClient = new QueryClient();
+
+// Sentry.init({
+//   dsn: import.meta.env.VITE_SENTRY_DSN,
+//   sendDefaultPii: true,
+//   enableLogs: true,
+//   integrations: [Sentry.replayIntegration()],
+//   replaysSessionSampleRate: 1.0,
+//   replaysOnErrorSampleRate: 1.0
+// });
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <App />
-    </ClerkProvider>
+    <BrowserRouter>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </ClerkProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
---
+++ admin/src/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+import {SignIn} from "@clerk/clerk-react";
+
+function LoginPage() {
+  return (
+    <div className="h-screen hero">
+      <SignIn />
+    </div>
+  );
+}
+
+export default LoginPage;
